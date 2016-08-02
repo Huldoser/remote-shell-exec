59687e4a9235312320df83f7945cb519bcb249a9
Andrei Richkov <huldoser@gmail.com>
2016-08-02 13:06:54 +0300

    Basic command execution works. Flags are not supported yet.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
-// *** This file respond to any call listed here ***
+// *** This file respond to all api call ***
 let express = require('express');
+
+let spawn = require('child_process').spawn;
 
 // Using express.Router() for modularity
 let router = express.Router();
@@ -9,8 +11,12 @@ router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// Get command from URL and execute. Respond with output as string.
 router.get('/:command', (req, res) => {
-    res.send('You entered ' + req.params);
+    let command = spawn(req.params.command.toString());
+    command.stdout.on('data', (data) => {
+        res.send(data.toString());
+    })
 });
 
 module.exports = router;
